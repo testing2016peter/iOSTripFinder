b2862a9df56ae31fa612d73a69134dd9f6de9bac
Anson Ng <cwnga@yahoo-inc.com>
2016-04-05 17:54:30 +0800

    add nav color

---
+++ Pods/Target Support Files/MNColorKit/MNColorKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MNColorKit : NSObject
+@end
+@implementation PodsDummy_MNColorKit
+@end
---
+++ tripFinder/Src/Util/Category/UIColor+APColor.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+APColor.m
+//  tripFinder
+//
+//  Created by Anson Ng on 4/4/16.
+//  Copyright © 2016 Anson Ng. All rights reserved.
+//
+
+#import "UIColor+APColor.h"
+
+@implementation UIColor (APColor)
+
+@end
